f6cfafc999f4a900d6c6b40b5fd35356d1e2da2a
DevelopmenTom <tomgivol@gmail.com>
2021-02-21 22:10:37 +0100

    fix(client): limit max size of menu body on deskop

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -57,7 +57,12 @@ const Index = ({ initialMenu }: Props) => {
         <meta charSet={'UTF-8'} />
       </Head>
       <Container h={'100%'} minHeight={'100vh'}>
-        <Box as={'main'} flex={'1'} minWidth={[null, null, '40%']}>
+        <Box
+          as={'main'}
+          flex={'1'}
+          maxWidth={[null, null, '80%']}
+          minWidth={[null, null, '40%']}
+        >
           {state.error && (
             <Alert zIndex={'2000'} status={'error'}>
               <AlertIcon />
